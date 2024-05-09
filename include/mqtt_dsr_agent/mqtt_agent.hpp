27aca8ff9af189b969d2c0df4c327f9e94502157
Alberto Tudela <ajtudela@gmail.com>
2024-05-09 12:17:40 +0200

    Initial commit
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>

---
+++ include/mqtt_dsr_agent/mqtt_agent.hpp
@@ -0,0 +1,48 @@
+// Copyright (c) 2024 Alberto J. Tudela Roldán
+// Copyright (c) 2024 Grupo Avispa, DTE, Universidad de Málaga
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef MQTT_AGENT_HPP_
+#define MQTT_AGENT_HPP_
+
+// Qt
+#include <QObject>
+
+#include "dsr/api/dsr_api.h"
+
+class MqttAgent : public QObject
+{
+  Q_OBJECT
+
+public:
+  MqttAgent(std::string agent_name, int agent_id);
+  virtual ~MqttAgent();
+
+  // DSR callbacks
+  void node_updated(std::uint64_t id, const std::string & type);
+  void node_attributes_updated(uint64_t id, const std::vector<std::string> & att_names);
+  void edge_updated(std::uint64_t from, std::uint64_t to, const std::string & type);
+  void edge_attributes_updated(
+    std::uint64_t from, std::uint64_t to,
+    const std::string & type, const std::vector<std::string> & att_names);
+  void node_deleted(std::uint64_t id);
+  void edge_deleted(std::uint64_t from, std::uint64_t to, const std::string & edge_tag);
+
+private:
+  std::string agent_name_;
+  int agent_id_;
+  std::shared_ptr<DSR::DSRGraph> G_;
+};
+
+#endif  // MQTT_AGENT_HPP_
---
+++ src/mqtt_agent.cpp
@@ -0,0 +1,71 @@
+// Copyright (c) 2024 Alberto J. Tudela Roldán
+// Copyright (c) 2024 Grupo Avispa, DTE, Universidad de Málaga
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "mqtt_dsr_agent/mqtt_agent.hpp"
+
+MqttAgent::MqttAgent(std::string agent_name, int agent_id)
+: agent_name_(agent_name), agent_id_(agent_id)
+{
+  G_ = std::make_shared<DSR::DSRGraph>(agent_name, agent_id, "");
+
+  // Add connection signals
+  QObject::connect(
+    G_.get(), &DSR::DSRGraph::update_node_signal, this, &MqttAgent::node_updated);
+  QObject::connect(
+    G_.get(), &DSR::DSRGraph::update_node_attr_signal, this, &MqttAgent::node_attributes_updated);
+  QObject::connect(
+    G_.get(), &DSR::DSRGraph::update_edge_signal, this, &MqttAgent::edge_updated);
+  QObject::connect(
+    G_.get(), &DSR::DSRGraph::update_edge_attr_signal, this, &MqttAgent::edge_attributes_updated);
+  QObject::connect(
+    G_.get(), &DSR::DSRGraph::del_node_signal, this, &MqttAgent::node_deleted);
+  QObject::connect(
+    G_.get(), &DSR::DSRGraph::del_edge_signal, this, &MqttAgent::edge_deleted);
+}
+
+MqttAgent::~MqttAgent()
+{
+  G_.reset();
+}
+
+void MqttAgent::node_updated(std::uint64_t /*id*/, const std::string & /*type*/)
+{
+
+}
+
+void MqttAgent::node_attributes_updated(uint64_t /*id*/, const std::vector<std::string> & /*att_names*/)
+{
+
+}
+
+void MqttAgent::edge_updated(std::uint64_t /*from*/, std::uint64_t /*to*/, const std::string & /*type*/)
+{
+
+}
+
+void MqttAgent::edge_attributes_updated(
+  std::uint64_t /*from*/, std::uint64_t /*to*/,
+  const std::string & /*type*/, const std::vector<std::string> & /*att_names*/)
+{
+
+}
+void MqttAgent::node_deleted(std::uint64_t /*id*/)
+{
+
+}
+void MqttAgent::edge_deleted(std::uint64_t /*from*/, std::uint64_t /*to*/, const std::string & /*edge_tag*/)
+{
+
+}
