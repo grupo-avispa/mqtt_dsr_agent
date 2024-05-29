170edb4e6a149a0ed1afd0d1bc0ebb6c8a2deb5e
Alberto Tudela <ajtudela@gmail.com>
2024-05-29 11:20:39 +0200

    Added public functions to mqttagent
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>

--- include/mqtt_dsr_agent/mqtt_agent.hpp
+++ include/mqtt_dsr_agent/mqtt_agent.hpp
@@ -39,6 +39,15 @@ public:
   void node_deleted(std::uint64_t id);
   void edge_deleted(std::uint64_t from, std::uint64_t to, const std::string & edge_tag);
 
+  template <typename node_type>
+  void insert_node<node_type>(const std::string & name);
+
+  template <typename edge_type>
+  void insert_edge<edge_type>(const std::string & from, const std::string & to);
+
+  template <typename att_type, typename value_type>
+  void insert_attribute<att_type>(const std::string & node, const value_type & value);
+
 private:
   std::string agent_name_;
   int agent_id_;
--- src/mqtt_agent.cpp
+++ src/mqtt_agent.cpp
@@ -40,6 +40,43 @@ MqttAgent::~MqttAgent()
   G_.reset();
 }
 
+// Functions to 'send' data to the DSR graph
+template <typename node_type>
+void MqttAgent::insert_node(const std::string &name)
+{
+  auto new_node = DSR::Node::create<node_type>(name);
+  if (auto id = G_->insert_node(new_node); id.has_value()){
+    std::cout << "Node " << name << " inserted with id " << id.value() << std::endl;
+  }
+}
+
+template <typename edge_type>
+void MqttAgent::insert_edge(const std::string &from, const std::string &to)
+{
+  // Get the relatives nodes
+  auto parent_node = G_->get_node(from);
+  auto child_node = G_->get_node(to);
+  if (parent_node.has_value()){
+    if (child_node.has_value()){
+      auto new_edge = DSR::Edge::create<edge_type>(parent_node.value().id(), child_node.value().id());
+      if (G_->insert_or_assign_edge(new_edge)){
+        std::cout << "Edge from " << from << " to " << to << " inserted" << std::endl;
+      }
+    }
+  }
+}
+
+template <typename att_type, typename value_type>
+void MqttAgent::insert_attribute(const std::string & node, const value_type & att_value)
+{
+  if (auto new_node = G_->get_node(node); new_node.has_value()){
+    G_->add_or_modify_attrib_local<att_type>(new_node.value(), att_value);
+    G_->update_node(new_node.value());
+    std::cout << "Attribute " << att_value << " inserted in node " << node << std::endl;
+  }
+}
+
+// Callbacks called when the DSR graph is changed
 void MqttAgent::node_updated(std::uint64_t /*id*/, const std::string & /*type*/)
 {
 
