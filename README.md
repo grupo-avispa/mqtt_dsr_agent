4e8b704077f77380fe7c0223c5f6a39c58c30dd0
Alberto Tudela <ajtudela@gmail.com>
2024-06-25 09:31:26 +0200

    Added paho cmake
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>

---
+++ README.md
@@ -0,0 +1,6 @@
+# mqtt_dsr_agent
+
+## Overview
+
+This is a simple MQTT agent that listens to a topic and sends the received messages to the DSR graph.
+
---
+++ cmake/pahomqtt.cmake
@@ -0,0 +1,23 @@
+if(NOT PahoMqttCpp_FOUND)
+    message(STATUS "Downloading paho_mqtt_cpp")
+    include(FetchContent)
+
+    set(PAHO_BUILD_SHARED ON CACHE INTERNAL "")
+    set(PAHO_BUILD_STATIC ON CACHE INTERNAL "")
+    set(PAHO_ENABLE_TESTING OFF CACHE INTERNAL "")
+    set(PAHO_BUILD_TESTS OFF CACHE INTERNAL "")
+    set(PAHO_WITH_MQTT_C ON CACHE INTERNAL "")
+
+    set(FETCHCONTENT_QUIET ON)
+    FetchContent_Declare(paho_mqtt_cpp
+        GIT_REPOSITORY    https://github.com/eclipse/paho.mqtt.cpp.git
+        GIT_TAG           v1.4.0
+        GIT_PROGRESS      TRUE
+        GIT_SHALLOW       TRUE
+        SOURCE_DIR "${CMAKE_CURRENT_BINARY_DIR}/paho_mqtt_cpp-src"
+        BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/paho_mqtt_cpp-build"
+    )
+    #FetchContent_MakeAvailable(paho_mqtt_cpp)
+    FetchContent_Populate(paho_mqtt_cpp)
+    add_subdirectory(${paho_mqtt_cpp_SOURCE_DIR} ${paho_mqtt_cpp_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif()
